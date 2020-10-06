d4e7f8d54ddffad464b5f8e0b396adacb113c2d8
BEAR80416@ICLOUD.COM <BEAR80416@ICLOUD.COM>
2020-10-06 15:11:15 +0800

    done

---
+++ WEBAPITOO/WEBAPITOO/Controllers/ToDoItemsController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WEBAPITOO.Data;
+using WEBAPITOO.Models;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Hosting;
+
+namespace WEBAPITOO.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoItemsController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _env;
+        private readonly ToDoContext _context;
+        private readonly ILogger<ToDoItemsController> _logger;
+
+        public ToDoItemsController(ToDoContext context,ILogger<ToDoItemsController> logger,IWebHostEnvironment env)
+        {
+            _env = env;
+            _context = context;
+            _logger = logger;
+        }
+
+        // GET: api/ToDoItems
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ToDoItem>>> GetTodoItems()
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+
+        // GET: api/ToDoItems/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ToDoItem>> GetToDoItem(long id)
+        {
+            var toDoItem = await _context.TodoItems.FindAsync(id);
+
+            if (toDoItem == null)
+            {
+                return NotFound();
+            }
+
+            return toDoItem;
+        }
+
+        // PUT: api/ToDoItems/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutToDoItem(long id, ToDoItem toDoItem)
+        {
+            if (id != toDoItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(toDoItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ToDoItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ToDoItems
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<ToDoItem>> PostToDoItem([FromForm]ToDoItem toDoItem)
+        {
+
+            if (_env.EnvironmentName == "Development")
+            {
+                string actionType = Request.Method;
+
+                _logger.LogWarning(2001, "post方法被呼叫,傳入資料為:" + System.Text.Json.JsonSerializer.Serialize(toDoItem));
+                _logger.LogWarning(2002, $"{actionType}方法被呼叫,傳入資料為:" + JsonConvert.SerializeObject(toDoItem));
+            }
+
+            _context.TodoItems.Add(toDoItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetToDoItem", new { id = toDoItem.Id }, toDoItem);
+        }
+
+        // DELETE: api/ToDoItems/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ToDoItem>> DeleteToDoItem(long id)
+        {
+            var toDoItem = await _context.TodoItems.FindAsync(id);
+            if (toDoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(toDoItem);
+            await _context.SaveChangesAsync();
+
+            return toDoItem;
+        }
+
+        private bool ToDoItemExists(long id)
+        {
+            return _context.TodoItems.Any(e => e.Id == id);
+        }
+    }
+}
